534cb6f6ce773eb7594ea0748e270bcffba0b0d0
blopezrodas <52397895+blopezrodas@users.noreply.github.com>
2019-07-12 21:45:59 -0700

    Update Player.java

--- app/src/main/java/com/gamecodeschool/asteroidsfs/Player.java
+++ app/src/main/java/com/gamecodeschool/asteroidsfs/Player.java
@@ -7,13 +7,14 @@ public class Player extends SpaceObjet {
 	private boolean hit;
 		
 	public Player() {
-		this(1,1,1,90);
+		this(1,1,1,1,false);
 	}
 	public Player(double pos_x, double pos_y) {
-		this(pos_x,pos_y,1,90);
+		this(pos_x,pos_y,1,1,false);
 	}
-	public Player(double pos_x, double pos_y, double speed, double direction, boolean hit) {
-		super(pos_x, pos_y, speed, direction);
+	public Player(double pos_x, double pos_y, double velocity_x, double velocity_y, boolean hit) {
+		super(pos_x, pos_y, velocity_x, velocity_y);
+		this.hit = hit;
 	}
 	
 	public void shoot() {
