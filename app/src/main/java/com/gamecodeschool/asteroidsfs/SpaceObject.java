dba0aa4ab3afbfdcad55bc72f09a5516ce6f1a03
Brenda Lopez <yohanalopezrodas@gmail.com>
2019-07-15 23:53:09 -0700

    SpaceObject: clean up

--- app/src/main/java/com/gamecodeschool/asteroidsfs/SpaceObject.java
+++ app/src/main/java/com/gamecodeschool/asteroidsfs/SpaceObject.java
@@ -5,25 +5,19 @@ import android.graphics.RectF;
 public class SpaceObject {
 
     private RectF mRect; // Give access to precise position and size of asteroid
-    private float mXVelocity;
-    private float mYVelocity;
-    private float mWidth;
-    private float mHeight;
+    private float xVelocity;
+    private float yVelocity;
+    private float width;
+    private float height;
 
-    SpaceObject(int screenX, int screenY) {
-        // Make the ball square and 1% of screen width
-        // of the screen width
-        mWidth = screenX / 100;
-        mHeight = screenX / 100;
-        // Initialize the RectF with 0, 0, 0, 0
-        // We do it here because we only want to
-        // do it once.
-        // We will initialize the detail
-        // at the start of each game
-        // default constructor sets variables (left, top, right and bottom) to zero
-        mRect = new RectF();
+
+    public SpaceObject(float xPosition, float yPosition, float width, float height, float xVelocity, float yVelocity) {
+        this.mRect = new RectF(xPosition, yPosition, xPosition+width,yPosition+height);
+        this.width = width;
+        this.height = height;
+        this.xVelocity = xVelocity;
+        this.yVelocity = yVelocity;
     }
-
 
 
     // Return a reference to mRect to AsteroidsGame
@@ -32,29 +26,15 @@ public class SpaceObject {
     }
 
 
-
-    // Update the asteroid position.
-    // Called each frame/loop
+    // Update the object's position (called each frame/loop)
+    // Move the object based on the velocity and current frame rate (mFPS)
     public void update(long fps){
-        // Move the object based upon the
-        // horizontal (mXVelocity) and
-        // vertical(mYVelocity) speed
-        // and the current frame rate(fps)
         // Move the top left corner
-        mRect.left = mRect.left + (mXVelocity / fps);
-        mRect.top = mRect.top + (mYVelocity / fps);
-
+        mRect.left = mRect.left + (xVelocity / fps);
+        mRect.top = mRect.top + (yVelocity / fps);
         // Match up the bottom right corner based on the size of the asteroid
-        mRect.right = mRect.left + mWidth;
-        mRect.bottom = mRect.top + mHeight;
-    }
-
-
-
-    public void increaseVelocity(){
-        // increase the speed by 10%
-        mXVelocity = mXVelocity * 1.1f;
-        mYVelocity = mYVelocity * 1.1f;
+        mRect.right = mRect.left + width;
+        mRect.bottom = mRect.top + height;
     }
 
 }
